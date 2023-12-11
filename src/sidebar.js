8ac0514e8b098761cdea82170e163fdc1eec7754
Mubashir <mubashir1728@gmail.com>
2023-12-11 10:56:18 +0530

    sidebar

---
+++ src/sidebar.js
@@ -0,0 +1,35 @@
+// sidebar.js
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+import "./sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <Menu
+      isOpen={true} // Always keep the menu open
+      pageWrapId={"page-wrap"}
+      outerContainerId={"outer-container"}
+    >
+      <a href="#" className="menu-item">
+        Home
+      </a>
+      <a href="#" className="menu-item">
+        Search
+      </a>
+      <a href="#" className="menu-item">
+        Contact
+      </a>
+      <a href="#" className="menu-item">
+        Send
+      </a>
+      <a href="#" className="menu-item">
+        Profile
+      </a>
+      <a href="#" className="menu-item">
+        Settings
+      </a>
+    </Menu>
+  );
+};
+
+export default Sidebar;
