8ed1d20d8b72e8004b4d6d63d4731fa051ad1c36
Mubashir <mubashir1728@gmail.com>
2023-12-20 20:48:18 +0530

    events API reponse fetching

--- src/components/eventCard.jsx
+++ src/components/eventCard.jsx
@@ -3,20 +3,57 @@ import "../css/eventCard.css"
 // import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 
 const EventComponent = () => {
-  const [timeLeft, setTimeLeft] = useState(113); // time in minutes
+  const [events, setEvents] = useState([]);
+  const [timeLeft, setTimeLeft] = useState(113); 
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://eceswebsitenodejs-production.up.railway.app/api/events');
+        const data = await response.json();
+
+        
+        setEvents(data.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData(); 
+
+    const timer = setInterval(() => {
       setTimeLeft(timeLeft - 1);
-    }, 6000);
-    return () => clearTimeout(timer);
-  });
+    }, 60000); 
+
+    
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timeLeft]);
   
   return (
-    <div className="event-component">
-      <h1>ECE-NITW Upcoming Event</h1>
-      <p>{timeLeft} minutes left</p>
-      <div className="rating">4.9/5</div>
+    <div className="event-component">  
+      <div className="event-list">
+        {events.map(event => (
+          <div key={event._id} className="event-card">
+            {event.photo && (
+              <img
+                src={event.photo}
+                alt={`Event: ${event.title}`}
+                style={{ width: '200px', height: 'auto', borderRadius: '15px' }}
+              />
+            )}
+            <div className="event-details">
+              <p>Title: {event.title}</p>
+              <p>Date and Time: {new Date(event.dateTime).toLocaleString()}</p>
+              <p>Location: {event.location}</p>
+              <p>Description: {event.description}</p>
+              <p>Target Audience: {event.targetAudience}</p>
+            </div>
+          </div>
+        ))}
+      </div>
       <button className="register-button">Register</button>
     </div>
   );
