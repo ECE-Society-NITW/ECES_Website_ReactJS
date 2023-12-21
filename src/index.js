c23f9d99167f8faed1a8c464b0ef5ec8acd78dfb
Mubashir <mubashir1728@gmail.com>
2023-12-21 16:28:02 +0530

    removed mongoose variable

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { AuthState } from './context/AuthContext';
 
 const clientId = "501179525954-ujf85hcbcr17l2e1elem6e1ra4ra55nh.apps.googleusercontent.com"
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const mongoose = require('mongoose');
 
 root.render(
   <React.StrictMode>
