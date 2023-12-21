67fecc3f72a3249221f975b4f44a7a2ecf251795
Mubashir <mubashir1728@gmail.com>
2023-12-21 16:58:01 +0530

    modifications in prev events container

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -39,6 +39,8 @@ const Dashboard = () => {
       </div>
       <div className='prev-event-cat'>
         <PrevEventCard />
+        <PrevEventCard />
+        <PrevEventCard />
       </div>
     </div>
   )
