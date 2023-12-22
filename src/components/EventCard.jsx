c0fac970c7d7d9b918c0741595585673b5b5ba8e
shashaank1453 <sd21ecb0a56@student.nitw.ac.in>
2023-12-22 16:59:41 +0530

    Don't remember what i changed

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -3,21 +3,22 @@ import { Code, LocationOn } from '@mui/icons-material'
 import React, { useEffect } from 'react'
 
 const EventCard = ({ data: { title, description, location, photo } }) => {
+
     useEffect(() => {
-        
+
     }, [])
+
     return (
         <>
 
             <Card sx={{ margin: 'auto', borderRadius: 6 }}>
-                <Stack direction='row' sx={{ width: '1000px', height: '270px', padding: '10px', backgroundColor: '#282828' }}>
-                    {/* <Paper elevation={20} sx={{ borderRadius: 6, width: '500px', height: '250px', borderRadius: 6 }}> */}
+                <Stack direction='row'
+                    sx={{ width: '1000px', height: '270px', padding: '10px', backgroundColor: '#282828' }}>
                     <CardMedia
                         sx={{ width: '500px', height: '250px', borderRadius: 6 }}
                         image={photo}
                         title="green iguana">
                     </CardMedia>
-                    {/* </Paper> */}
                     <CardContent sx={{ height: '100%', width: '700px' }}>
                         <Stack direction='column' sx={{ height: '100%', justifyContent: 'space-between', }} >
                             <Typography gutterBottom variant="h5" color='white' component="div">
