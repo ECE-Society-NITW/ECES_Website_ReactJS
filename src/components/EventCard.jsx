be3a35ec6843a7ad11e9bea1e280996f4b89a2de
shashaank1453 <sd21ecb0a56@student.nitw.ac.in>
2023-12-22 17:51:51 +0530

    Revert "Issues fixed"
    This reverts commit 45a787ec8d31b174963c5a8b56c6b6e50da083db.

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 import { Card, CardMedia, Chip, Typography, CardContent, CardActions, Button, Box, Stack } from '@mui/material'
-import { Code, LocationOn, Phone } from '@mui/icons-material'
-=======
-import { Card, CardMedia, Chip, Typography, CardContent, CardActions, Button, Box, Stack, Paper } from '@mui/material'
 import { Code, LocationOn } from '@mui/icons-material'
->>>>>>> parent of 45a787e (Issues fixed)
 import React, { useEffect } from 'react'
 
 const EventCard = ({ data: { title, description, location, photo } }) => {
     useEffect(() => {
-        console.log(photo)
+        
     }, [])
     return (
         <>
@@ -17,11 +12,11 @@ const EventCard = ({ data: { title, description, location, photo } }) => {
             <Card sx={{ margin: 'auto', borderRadius: 6 }}>
                 <Stack direction='row' sx={{ width: '1000px', height: '270px', padding: '10px', backgroundColor: '#282828' }}>
                     {/* <Paper elevation={20} sx={{ borderRadius: 6, width: '500px', height: '250px', borderRadius: 6 }}> */}
-                        <CardMedia
-                            sx={{ width: '500px', height: '250px', borderRadius: 6 }}
-                            image={photo}
-                            title="green iguana">
-                        </CardMedia>
+                    <CardMedia
+                        sx={{ width: '500px', height: '250px', borderRadius: 6 }}
+                        image={photo}
+                        title="green iguana">
+                    </CardMedia>
                     {/* </Paper> */}
                     <CardContent sx={{ height: '100%', width: '700px' }}>
                         <Stack direction='column' sx={{ height: '100%', justifyContent: 'space-between', }} >
@@ -39,10 +34,9 @@ const EventCard = ({ data: { title, description, location, photo } }) => {
                     <CardContent sx={{ height: '100%', width: '300px' }}>
                         <Stack>
                             <Box>
-                                <Chip icon={<Code />} label="Software" margin={1}/>
-                                <Chip icon={<LocationOn />} label="TimeSquare" margin={1}/>
-                                <Chip icon={<Code />} label="Software" variant="outlined" margin={1}/>
-                                <Chip icon={<Phone />} label="Contact" variant="outlined" margin={1}/>
+                                <Chip icon={<Code />} label="Software" margin={3}/>
+                                <Chip icon={<LocationOn />} label="TimeSquare" margin={3}/>
+                                <Chip icon={<Code />} label="Software" variant="outlined" margin={3}/>
                             </Box>
                         </Stack>
                     </CardContent>
