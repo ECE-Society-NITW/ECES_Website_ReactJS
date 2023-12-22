da3559252166da0753aefd135c8e3573555ce234
shashaank1453 <sd21ecb0a56@student.nitw.ac.in>
2023-12-23 00:29:56 +0530

    Removed unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Members from './pages/Members';
 import { useContextAuth } from './context/AuthContext';
