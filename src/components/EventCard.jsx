943ed6d1d726d849bd0393098f00ac3fb4e04093
shashaank1453 <sd21ecb0a56@student.nitw.ac.in>
2023-12-22 17:08:26 +0530

    dfsdgffg

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,10 +1,15 @@
+<<<<<<< HEAD
 import { Card, CardMedia, Chip, Typography, CardContent, CardActions, Button, Box, Stack } from '@mui/material'
 import { Code, LocationOn, Phone } from '@mui/icons-material'
+=======
+import { Card, CardMedia, Chip, Typography, CardContent, CardActions, Button, Box, Stack, Paper } from '@mui/material'
+import { Code, LocationOn } from '@mui/icons-material'
+>>>>>>> parent of 45a787e (Issues fixed)
 import React, { useEffect } from 'react'
 
 const EventCard = ({ data: { title, description, location, photo } }) => {
     useEffect(() => {
-        
+        console.log(photo)
     }, [])
     return (
         <>
@@ -12,11 +17,11 @@ const EventCard = ({ data: { title, description, location, photo } }) => {
             <Card sx={{ margin: 'auto', borderRadius: 6 }}>
                 <Stack direction='row' sx={{ width: '1000px', height: '270px', padding: '10px', backgroundColor: '#282828' }}>
                     {/* <Paper elevation={20} sx={{ borderRadius: 6, width: '500px', height: '250px', borderRadius: 6 }}> */}
-                    <CardMedia
-                        sx={{ width: '500px', height: '250px', borderRadius: 6 }}
-                        image={photo}
-                        title="green iguana">
-                    </CardMedia>
+                        <CardMedia
+                            sx={{ width: '500px', height: '250px', borderRadius: 6 }}
+                            image={photo}
+                            title="green iguana">
+                        </CardMedia>
                     {/* </Paper> */}
                     <CardContent sx={{ height: '100%', width: '700px' }}>
                         <Stack direction='column' sx={{ height: '100%', justifyContent: 'space-between', }} >
