a9e723f412de4477db6d1fe89ec0e4d5576de6d5
shashaank1453 <sd21ecb0a56@student.nitw.ac.in>
2023-12-23 02:43:37 +0530

    Default DarkMode enabled

--- src/context/modeContext.jsx
+++ src/context/modeContext.jsx
@@ -1,10 +1,14 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useEffect } from 'react'
 import { useColorScheme } from '@mui/material'
 const ModeContext = createContext()
 
 const ModeState = ({ children }) => {
 
     const { mode, setMode } = useColorScheme()
+
+    useEffect(()=>{
+        setMode('dark')
+    })
 
     return (
         <ModeContext.Provider value={{mode,setMode}}>
